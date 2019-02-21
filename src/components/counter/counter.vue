f0f4577e2311d3da2472d3baa7f2d7ad63074fb6
Sergio Enrique Ruiz Davila <sergioruizdavila@gmail.com>
2019-02-22 05:04:29 +1300

    Mod: Added Counter component (Logic)

--- src/components/counter/counter.vue
+++ src/components/counter/counter.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="val-counter">
     <div class="digit digit--day d-inline-flex flex-column">
-      <span class="text digit__number">22</span>
+      <span class="text digit__number">{{addLeadingZeros(date.days)}}</span>
       <span class="text digit__label">Dia</span>
     </div>
     <div class="digit digit--hour d-inline-flex flex-column">
-      <span class="text digit__number">22</span>
+      <span class="text digit__number">{{addLeadingZeros(date.hours)}}</span>
       <span class="text digit__label">Hrs.</span>
     </div>
     <div class="digit digit--min d-inline-flex flex-column">
-      <span class="text digit__number">22</span>
+      <span class="text digit__number">{{addLeadingZeros(date.min)}}</span>
       <span class="text digit__label">Min.</span>
     </div>
     <div class="digit digit--sec d-inline-flex flex-column">
-      <span class="text digit__number">22</span>
+      <span class="text digit__number">{{addLeadingZeros(date.sec)}}</span>
       <span class="text digit__label">Seg.</span>
     </div>
   </div>
@@ -23,6 +23,82 @@
   import './counter.scss';
 
   export default {
-    name: 'val-counter'
+    name: 'val-counter',
+    props: {
+      deadline: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        date: {
+          days: 0,
+          hours: 0,
+          min: 0,
+          sec: 0
+        }
+      };
+    },
+    mounted() {
+      // update every second
+      this.interval = setInterval(() => {
+        const date = this.calculateCountdown(this.deadline);
+        console.log('date: ', date);
+        if(date) {
+          this.date = date;
+        } else { this.stop() }
+      }, 1000);
+    },
+    methods: {
+      calculateCountdown(endDate) {
+        let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+
+        // clear countdown when date is reached
+        if (diff <= 0) return false;
+
+        const timeLeft = {
+          years: 0,
+          days: 0,
+          hours: 0,
+          min: 0,
+          sec: 0,
+          millisec: 0,
+        };
+
+        // calculate time difference between now and expected date
+        if (diff >= (365.25 * 86400)) { // 365.25 * 24 * 60 * 60
+          timeLeft.years = Math.floor(diff / (365.25 * 86400));
+          diff -= timeLeft.years * 365.25 * 86400;
+        }
+        if (diff >= 86400) { // 24 * 60 * 60
+          timeLeft.days = Math.floor(diff / 86400);
+          diff -= timeLeft.days * 86400;
+        }
+        if (diff >= 3600) { // 60 * 60
+          timeLeft.hours = Math.floor(diff / 3600);
+          diff -= timeLeft.hours * 3600;
+        }
+        if (diff >= 60) {
+          timeLeft.min = Math.floor(diff / 60);
+          diff -= timeLeft.min * 60;
+        }
+        timeLeft.sec = diff;
+
+        return timeLeft;
+      },
+
+      addLeadingZeros(value) {
+        value = String(value);
+        while (value.length < 2) {
+          value = '0' + value;
+        }
+        return value;
+      },
+
+      stop() {
+        clearInterval(this.interval);
+      }
+
+    }
   };
 </script>
